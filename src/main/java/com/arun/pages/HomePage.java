fa5088b2988796655815ca66a776ff6b8779c1d2
gurram arun <gurramarun1911@gmail.com>
2026-03-12 14:43:23 +0530

    updated all files

--- src/main/java/com/arun/pages/HomePage.java
+++ src/main/java/com/arun/pages/HomePage.java
@@ -14,25 +14,31 @@ public class HomePage {
     private By findFlightsBtn = By.xpath("//input[@value='Find Flights']");
 
     public HomePage(WebDriver driver) {
-
         this.driver = driver;
-
     }
 
     // Select Departure City
     public void selectDepartureCity(String city) {
 
         Select depart = new Select(driver.findElement(departureCity));
-        depart.selectByVisibleText(city);
 
+        try {
+            depart.selectByVisibleText(city);
+        } catch (Exception e) {
+            System.out.println("City not found in dropdown: " + city);
+        }
     }
 
     // Select Destination City
     public void selectDestinationCity(String city) {
 
         Select destination = new Select(driver.findElement(destinationCity));
-        destination.selectByVisibleText(city);
 
+        try {
+            destination.selectByVisibleText(city);
+        } catch (Exception e) {
+            System.out.println("City not found in dropdown: " + city);
+        }
     }
 
     // Click Find Flights
